ff93d41f57d55c2f1d792a62cf121a14265cd257
Pedro Escapalete <pedroescapalete@gmail.com>
2026-05-15 11:09:00 -0300

    ft: melhora o layout na pagina VideoAula

--- src/pages/VideoAula/VideoAula.jsx
+++ src/pages/VideoAula/VideoAula.jsx
@@ -5,40 +5,40 @@ import Menu from '../../components/Menu/Menu';
 import Footer from '../../components/Footer/Footer';
 import { LanguageContext } from '../../contexts/LanguageContext';
 
-const texts= {
-    "pt-br": {
-        titulo: "Videos Aulas",
-        subtitulo: "Aprenda com nossas video-aulas! Veja a seguir nossa lista de videos e reviews sobre os livros",
-        tituloVideo1: "Titulo do Video 1",
-        subtituloVideo1: "Subtitulo do Video 1",
-        descricaoVideo1: "Descrição do video 1",
-        tituloVideo2: "Titulo do Video 2",
-        subtituloVideo2: "Subtitulo do video 2",
-        descricaoVideo2: "Descrição do video 2",
-        tituloVideo3: "Titulo do video 3",
-        subtituloVideo3: "Subtitulo do Video 3",
-        descricaoVideo3: "Descrição do video 3",
-        tituloVideo4: "Titulo do video 4",
-        subtituloVideo4: "Subtitulo do video 4",
-        descricaoVideo4: "Descrição do video 4"
+const texts = {
+    'pt-br': {
+        titulo: 'Videos Aulas',
+        canal: 'Aprenda com nossas video-aulas! Veja a seguir nossa lista de videos e reviews sobre os livros',
+        tituloVideo1: 'Titulo do Video 1',
+        canal1: 'canal do Video 1',
+        descricaoVideo1: 'Descrição do video 1',
+        tituloVideo2: 'Titulo do Video 2',
+        canal2: 'canal do video 2',
+        descricaoVideo2: 'Descrição do video 2',
+        tituloVideo3: 'Titulo do video 3',
+        canal3: 'canal do Video 3',
+        descricaoVideo3: 'Descrição do video 3',
+        tituloVideo4: 'Titulo do video 4',
+        canal4: 'canal do video 4',
+        descricaoVideo4: 'Descrição do video 4',
     },
-    "en": {
-        titulo: "Video lesson",
-        subtitulo: "Learn with our video lessons! See below our list of videos and book reviews",
-        tituloVideo1: "Video 1 Title",
-        subtituloVideo1: "Video 1 Subtitle",
-        descricaoVideo1: "Video 1 Description",
-        tituloVideo2: "Video 2 Title",
-        subtituloVideo2: "Video 2 Subtitle",
-        descricaoVideo2: "Video 2 Description",
-        tituloVideo3: "Video 3 Title",
-        subtituloVideo3: "Video 3 Subtitle",
-        descricaoVideo3: "Video 3 Description",
-        tituloVideo4: "Video 4 Title",
-        subtituloVideo4: "Video 4 Subtitle",
-        descricaoVideo4: "Video 4 Description"
-    }
-}
+    en: {
+        titulo: 'Video lesson',
+        canal: 'Learn with our video lessons! See below our list of videos and book reviews',
+        tituloVideo1: 'Video 1 Title',
+        canal1: 'Video 1 channel',
+        descricaoVideo1: 'Video 1 Description',
+        tituloVideo2: 'Video 2 Title',
+        canal2: 'Video 2 channel',
+        descricaoVideo2: 'Video 2 Description',
+        tituloVideo3: 'Video 3 Title',
+        canal3: 'Video 3 channel',
+        descricaoVideo3: 'Video 3 Description',
+        tituloVideo4: 'Video 4 Title',
+        canal4: 'Video 4 channel',
+        descricaoVideo4: 'Video 4 Description',
+    },
+};
 
 export default function Videos() {
     const context = useContext(LanguageContext);
@@ -65,26 +65,26 @@ export default function Videos() {
                         <h1 className={styles.titulo}>
                             The Rats — <em className={styles.destaque}>{t.titulo}</em>
                         </h1>
-                        <p className={styles.subtitulo}>{t.subtitulo}</p>
+                        <p className={styles.canal}>{t.canal}</p>
                     </div>
                 </section>
                 <section className={styles.videos}>
                     <div className={styles.video}>
                         <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
                         <h2 className={styles.tituloVideo}>{t.tituloVideo1}</h2>
-                        <p className={styles.subtituloVideo}>{t.subtituloVideo1}</p>
+                        <p className={styles.canal}>{t.canal1}</p>
                         <p className={styles.descricao}>{t.descricaoVideo1}</p>
                     </div>
                     <div className={styles.video}>
                         <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
                         <h2 className={styles.tituloVideo}>{t.tituloVideo2}</h2>
-                        <p className={styles.subtituloVideo}>{t.subtituloVideo2}</p>
+                        <p className={styles.canal}>{t.canal2}</p>
                         <p className={styles.descricao}>{t.descricaoVideo2}</p>
                     </div>
                     <div className={styles.video}>
                         <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>
                         <h2 className={styles.tituloVideo}>{t.tituloVideo3}</h2>
-                        <p className={styles.subtituloVideo}>{t.subtituloVideo3}</p>
+                        <p className={styles.canal}>{t.canal3}</p>
                         <p className={styles.descricao}>{t.descricaoVideo3}</p>
                     </div>
                 </section>
